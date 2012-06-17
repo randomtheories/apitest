98f5d894a71d344bf6bade9efb10696c775f38b2
randomtheories <jdisselhoff@gmail.com>
2012-06-17 18:18:38 +0200

    Added root-route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Apitest::Application.routes.draw do
   resources :pages
   match 'api' => 'api#getnames'
+  root :to => 'pages#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
